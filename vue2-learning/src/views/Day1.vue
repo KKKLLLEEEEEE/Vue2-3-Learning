56cc0d542d04866bd094b3b33e3a7e2862a0fce6
Genshin Master <mianjiezhou@gmail.com>
2024-11-24 23:21:57 +0300

    initial day 1 branch

--- vue2-learning/src/views/Day1.vue
+++ vue2-learning/src/views/Day1.vue
@@ -1,33 +1,26 @@
 <template>
-  <div class="day1">
-    <h1>Vue.js 第一天学习</h1>
-    <button @click="$router.push('/')">返回首页</button>
+  <div>
+    {{ message + message2.toUpperCase() }}
+    <br>
+    <button @click="count++">点击：{{ count }}</button>
+    <p>{{ GenshinImpact.name }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DayOneStudy'
+  name: 'DayOneStudy',
+  data() {
+    return {
+      message: 'Hello World!',
+      message2: 'Hello Vue2!',
+      count: 0,
+      GenshinImpact:{
+        name: '旅行者',
+        age: 18,
+        gender: '男'
+      }
+    }
+  }
 }
-</script>
-
-<style scoped>
-.day1 {
-  padding: 20px;
-}
-
-button {
-  padding: 10px 20px;
-  font-size: 16px;
-  background-color: #42b983;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin: 10px 0;
-}
-
-button:hover {
-  background-color: #3aa876;
-}
-</style> 
+</script> 
