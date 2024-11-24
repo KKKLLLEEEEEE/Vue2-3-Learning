5170db5f0941792cdddd04abbcfa7ec26dd55ad8
Genshin Master <mianjiezhou@gmail.com>
2024-11-25 00:28:49 +0300

    newwork die, i did not carry my phont

--- vue2-learning/src/views/Day1.vue
+++ vue2-learning/src/views/Day1.vue
@@ -4,6 +4,17 @@
     <br>
     <button @click="count++">点击：{{ count }}</button>
     <p>{{ GenshinImpact.name }}</p>
+    <p><a :href="link">Google1</a></p>
+    <p v-html="link2"></p>
+    <button v-on:click="count2++">v-on click++  {{ count2 }}</button>
+    <button @click="count2--">v-on click--  {{ count2 }}</button>
+    <br>
+    <button @click="toShow">click to show or hide</button>
+    <span v-show="showFlag">suprise!</span>
+    <br>
+    <span v-bind:style="color">hide</span>
+
+    
   </div>
 </template>
 
@@ -19,7 +30,21 @@ export default {
         name: '旅行者',
         age: 18,
         gender: '男'
-      }
+      },
+      link: 'http://www.google.com',
+      link2: '<a href="http://www.google.com">Google2</a>',
+      count2: 0,
+      showFlag: false,
+      hideFlag: true,
+      color: 'red'
+    }
+  },
+  methods: {
+    toShow() {
+        this.showFlag = !this.showFlag
+    },
+    calculate(num1,num2) {
+        return num1 + num2
     }
   }
 }
