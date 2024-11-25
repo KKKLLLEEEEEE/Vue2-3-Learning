cee9936967038545009bc6839dd31e6219eb1f3b
Genshin Master <mianjiezhou@gmail.com>
2024-11-26 00:58:19 +0300

    day2 finish, maybe...

--- vue2-learning/src/views/Day2.vue
+++ vue2-learning/src/views/Day2.vue
@@ -1,16 +1,61 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <div :class="{'box': true, 'color': true, 'margin': true}">
+      <h1>Hello World</h1>
+      <br>
+      <input type="text" v-model="num1">
+      +
+      <input type="text" v-model="num2">
+      =
+      <span>{{ sum }}</span>
+      <br>
+      <button @click="changeNum">change num</button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+}
+.color {
+  color: pink;
+}
+.margin {
+  margin: 0 auto;
+}
+</style>
 
 <script>
 export default {
   name: 'DayTwoStudy',
   data() {
     return {
-      message: 'Hello World from Day 2!'
+      num1: 0,
+      num2: 0
+    }
+  },
+  computed: {
+    sum: {
+      
+        get() {
+            return this.num1 + this.num2;
+        },
+        set(value) {
+            this.num1 = 999;
+            this.num2 = 999;
+        }       
+    
+    }
+  },
+  methods: {
+    changeNum() {
+       //how to change sum:sum2:set
+       this.sum=1;
     }
   }
+  
+  
 }
 </script> 
